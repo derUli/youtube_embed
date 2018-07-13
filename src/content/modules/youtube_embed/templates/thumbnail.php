a4a29caefdb8293629586802151756a4368e21ad
Ulrich Schmidt <support@ulicms.de>
2018-07-13 20:53:25 +0200

    target blank

--- src/content/modules/youtube_embed/templates/thumbnail.php
+++ src/content/modules/youtube_embed/templates/thumbnail.php
@@ -4,6 +4,6 @@ $youtubeUrl = "https://www.youtube.com/watch?v={$videoId}";
 $actionURL = ModuleHelper::buildMethodCallUrl("YoutubeEmbed", "thumbnail", "url=" . urlencode($youtubeUrl));
 ?>
 <a href="<?php esc($youtubeUrl);?>"
-	title="<?php translate("watch_on_youtube");?>"> <img
+	title="<?php translate("watch_on_youtube");?>" target="_blank"> <img
 	src="<?php esc($actionURL);?>"
 	alt="<?php translate("watch_on_youtube");?>"></a>
