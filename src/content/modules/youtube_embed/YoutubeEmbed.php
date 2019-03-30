71785dffa9518b78a97b02120ca5d9d3b26a655a
Ulrich Schmidt <admin@deruli.de>
2019-03-30 16:37:53 +0100

    support for youtube.be URLs added

--- src/content/modules/youtube_embed/YoutubeEmbed.php
+++ src/content/modules/youtube_embed/YoutubeEmbed.php
@@ -1,28 +1,24 @@
 <?php
 
-class YoutubeEmbed extends MainClass
-{
+class YoutubeEmbed extends MainClass {
 
     const MODULE_NAME = "youtube_embed";
 
-    public function head()
-    {
+    public function head() {
         enqueueStylesheet(ModuleHelper::buildModuleRessourcePath(self::MODULE_NAME, "css/youtube.css"));
         combinedStylesheetHtml();
     }
 
-    public function adminHead()
-    {
+    public function adminHead() {
         $this->head();
     }
 
-    public function contentFilter($html)
-    {
+    public function contentFilter($html) {
         $youtube_embed_layout = Settings::get("youtube_embed_layout", "str");
-        if (! $youtube_embed_layout) {
+        if (!$youtube_embed_layout) {
             $youtube_embed_layout = "player";
         }
-        
+
         preg_match_all("/\[youtube=(.+)]/i", $html, $matches);
         if (count($matches[0]) > 0) {
             for ($i = 0; $i < count($matches[0]); $i ++) {
@@ -35,57 +31,63 @@ class YoutubeEmbed extends MainClass
         return $html;
     }
 
-    public function thumbnail()
-    {
+    public function thumbnail() {
         $url = Request::getVar("url");
         $number = Request::getVar("number") ? Request::getVar("number") : 0;
-        
-        $query = parse_url($url, PHP_URL_QUERY);
-        $args = array();
-        parse_str($query, $args);
-        $videoId = isset($args["v"]) ? $args["v"] : null;
+
+        $videoId = $this->getVideoId($url);
+
         $thumbnailUrl = "https://img.youtube.com/vi/{$videoId}/{$number}.jpg";
-        
+
         $image = file_get_contents_wrapper($thumbnailUrl, false);
-        
-        if (! $image) {
+
+        if (!$image) {
             TextResult("NotFound", HttpStatusCode::NOT_FOUND);
         }
         Result($image, HttpStatusCode::OK, "image/jpeg");
     }
 
-    public function getYoutubeEmbedHtml($url, $layout = "player")
-    {
+    public function getVideoId($url) {
+        $videoId = null;
         $query = parse_url($url, PHP_URL_QUERY);
-        $args = array();
-        parse_str($query, $args);
-        $videoId = isset($args["v"]) ? $args["v"] : null;
-        if (! $videoId) {
-            return null;
+        $domain = parse_url($url, PHP_URL_HOST);
+
+        if (str_contains("youtu.be", $domain)) {
+            $path = parse_url($url, PHP_URL_PATH);
+            $videoId = basename($path);
+        } else {
+            $args = array();
+            parse_str($query, $args);
+            $videoId = isset($args["v"]) ? $args["v"] : null;
         }
+        return $videoId;
+    }
+
+    public function getYoutubeEmbedHtml($url, $layout = "player") {
+        $videoId = $this->getVideoId($url);
+
         ViewBag::set("video_id", $videoId);
         return Template::executeModuleTemplate(self::MODULE_NAME, "{$layout}.php");
     }
 
-    public function settings()
-    {
+    public function settings() {
         if (Request::isPost()) {
             $youtube_embed_layout = Request::getVar("youtube_embed_layout");
-            if (! faster_in_array($youtube_embed_layout, array(
-                "player",
-                "thumbnail"
-            ))) {
+            if (!faster_in_array($youtube_embed_layout, array(
+                        "player",
+                        "thumbnail"
+                    ))) {
                 $youtube_embed_layout = "player";
             }
             Settings::set("youtube_embed_layout", $youtube_embed_layout);
             Request::redirect(ModuleHelper::buildAdminURL(self::MODULE_NAME, "save=1"));
         }
-        
+
         return Template::executeModuleTemplate(self::MODULE_NAME, "settings.php");
     }
 
-    public function getSettingsHeadline()
-    {
+    public function getSettingsHeadline() {
         return '<i class="fab fa-youtube text-red"></i> Youtube Embed';
     }
+
 }
