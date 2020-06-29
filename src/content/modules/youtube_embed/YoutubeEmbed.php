dab04b5f151816ca51890f63d4697be46984ef18
Der Uli im Netz <admin@deruli.de>
2020-06-29 20:02:54 +0200

    update

--- src/content/modules/youtube_embed/YoutubeEmbed.php
+++ src/content/modules/youtube_embed/YoutubeEmbed.php
@@ -1,19 +1,22 @@
 <?php
 
-class YoutubeEmbed extends MainClass {
-
+class YoutubeEmbed extends MainClass
+{
     const MODULE_NAME = "youtube_embed";
 
-    public function head() {
+    public function head(): void
+    {
         enqueueStylesheet(ModuleHelper::buildModuleRessourcePath(self::MODULE_NAME, "css/youtube.css"));
         combinedStylesheetHtml();
     }
 
-    public function adminHead() {
+    public function adminHead(): void
+    {
         $this->head();
     }
 
-    public function contentFilter($html) {
+    public function contentFilter($html): string
+    {
         // special case for blog module
         $blog_admin = Request::getVar("blog_admin");
         if (in_array($blog_admin, [
@@ -28,7 +31,7 @@ class YoutubeEmbed extends MainClass {
 
         preg_match_all("/\[youtube=(.+)]/i", $html, $matches);
         if (count($matches[0]) > 0) {
-            for ($i = 0; $i < count($matches[0]); $i ++) {
+            for ($i = 0; $i < count($matches[0]); $i++) {
                 $replaceCode = $matches[0][$i];
                 $url = strip_tags($matches[1][$i]);
                 $embedCode = $this->getYoutubeEmbedHtml($url, $youtube_embed_layout);
@@ -38,15 +41,9 @@ class YoutubeEmbed extends MainClass {
         return $html;
     }
 
-    public function thumbnail() {
-        $url = Request::getVar("url");
-        $number = Request::getVar("number") ? Request::getVar("number") : 0;
-
-        $videoId = $this->getVideoId($url);
-
-        $thumbnailUrl = "https://img.youtube.com/vi/{$videoId}/{$number}.jpg";
-
-        $image = file_get_contents_wrapper($thumbnailUrl, false);
+    public function thumbnail(): void
+    {
+        $image = $this->_thumbnail();
 
         if (!$image) {
             TextResult("NotFound", HttpStatusCode::NOT_FOUND);
@@ -54,30 +51,56 @@ class YoutubeEmbed extends MainClass {
         Result($image, HttpStatusCode::OK, "image/jpeg");
     }
 
-    public function getVideoId($url) {
+    public function _thumbnail(): ?string
+    {
+        $url = Request::getVar("url", "", "str");
+        $number = Request::getVar("number", 0, "int");
+
+        $videoId = $this->getVideoId($url);
+        $thumbnailUrl = "https://img.youtube.com/vi/{$videoId}/{$number}.jpg";
+
+        $image = file_get_contents_wrapper($thumbnailUrl, false);
+        if (!$image) {
+            return null;
+        }
+        
+        return $image;
+    }
+
+    public function getVideoId(string $url): ?string
+    {
         $videoId = null;
         $query = parse_url($url, PHP_URL_QUERY);
         $domain = parse_url($url, PHP_URL_HOST);
 
-        if (str_contains("youtu.be", $domain)) {
+        if (!$domain) {
+            return null;
+        }
+
+        if (str_contains($domain, "youtu.be")) {
             $path = parse_url($url, PHP_URL_PATH);
             $videoId = basename($path);
         } else {
-            $args = array();
+            $args = [];
             parse_str($query, $args);
             $videoId = isset($args["v"]) ? $args["v"] : null;
         }
         return $videoId;
     }
 
-    public function getYoutubeEmbedHtml($url, $layout = "player") {
+    public function getYoutubeEmbedHtml(string $url, string $layout = "player")
+    {
         $videoId = $this->getVideoId($url);
 
         ViewBag::set("video_id", $videoId);
-        return Template::executeModuleTemplate(self::MODULE_NAME, "{$layout}.php");
+        return Template::executeModuleTemplate(
+            self::MODULE_NAME,
+            "{$layout}.php"
+        );
     }
 
-    public function settings() {
+    public function settings(): ?string
+    {
         if (Request::isPost()) {
             $youtube_embed_layout = Request::getVar("youtube_embed_layout");
             if (!faster_in_array($youtube_embed_layout, array(
@@ -87,14 +110,19 @@ class YoutubeEmbed extends MainClass {
                 $youtube_embed_layout = "player";
             }
             Settings::set("youtube_embed_layout", $youtube_embed_layout);
-            Request::redirect(ModuleHelper::buildAdminURL(self::MODULE_NAME, "save=1"));
+            Request::redirect(
+                ModuleHelper::buildAdminURL(self::MODULE_NAME, "save=1")
+            );
         }
 
-        return Template::executeModuleTemplate(self::MODULE_NAME, "settings.php");
+        return Template::executeModuleTemplate(
+            self::MODULE_NAME,
+            "settings.php"
+        );
     }
 
-    public function getSettingsHeadline() {
+    public function getSettingsHeadline(): string
+    {
         return '<i class="fab fa-youtube text-red"></i> Youtube Embed';
     }
-
 }
--- src/content/modules/youtube_embed/tests/YoutubeEmbedTest.php
+++ src/content/modules/youtube_embed/tests/YoutubeEmbedTest.php
@@ -1,28 +1,124 @@
 <?php
 
 use PHPUnit\Framework\TestCase;
+use Spatie\Snapshots\MatchesSnapshots;
+use UliCMS\Helpers\TestHelper;
 
-class YoutubeEmbedTest extends TestCase {
+class YoutubeEmbedTest extends TestCase
+{
+    use MatchesSnapshots;
+
+    protected function setUp(): void
+    {
+        Translation::loadAllModuleLanguageFiles("en");
+    }
+
+    protected function tearDown(): void
+    {
+        $_GET = [];
+    }
 
     // youtube.com/watch?v=xyz
-    public function testGetVideoIdFromFullYoutubeUrl() {
+    public function testGetVideoIdFromFullYoutubeUrl()
+    {
         $videoUrl = "https://www.youtube.com/watch?v=d6sfgRTEzjI";
-        $controller = ModuleHelper::getMainController("youtube_embed");
+        $controller = new YoutubeEmbed();
         $this->assertEquals("d6sfgRTEzjI", $controller->getVideoId($videoUrl));
     }
 
     // youtu.be/xyz
-    public function testGetVideoIdFromShortYoutubeUrl() {
+    public function testGetVideoIdFromShortYoutubeUrl()
+    {
         $videoUrl = "https://youtu.be/qyqWGSmo9PY";
-        $controller = ModuleHelper::getMainController("youtube_embed");
+        $controller = new YoutubeEmbed();
         $this->assertEquals("qyqWGSmo9PY", $controller->getVideoId($videoUrl));
     }
 
     // Invalid url returns null
-    public function testGetVideoIdFromInvalidUrl() {
+    public function testGetVideoIdFromInvalidUrl()
+    {
         $videoUrl = "http://this-url-is-crap.com/?a=213213123123";
-        $controller = ModuleHelper::getMainController("youtube_embed");
+        $controller = new YoutubeEmbed();
+        
         $this->assertNull($controller->getVideoId($videoUrl));
     }
 
+    public function testGetYoutubeEmbedHtml()
+    {
+        $videoUrl = "https://youtu.be/qyqWGSmo9PY";
+        $controller = new YoutubeEmbed();
+        $html = $controller->getYoutubeEmbedHtml($videoUrl);
+        $this->assertMatchesHtmlSnapshot($html);
+    }
+
+    public function testContentFilter()
+    {
+        $input = "Foo [youtube=https://youtu.be/qyqWGSmo9PY] Bar";
+        $controller = new YoutubeEmbed();
+        $html = $controller->contentFilter($input);
+        $this->assertMatchesHtmlSnapshot($html);
+    }
+    public function testContentFilterOnBlogPage()
+    {
+        $_GET["blog_admin"] = "add";
+        $input = "Foo [youtube=https://youtu.be/qyqWGSmo9PY] Bar";
+        $controller = new YoutubeEmbed();
+        $html = $controller->contentFilter($input);
+        $this->assertMatchesHtmlSnapshot($html);
+    }
+
+    public function testGetSettingsHeadline()
+    {
+        $controller = new YoutubeEmbed();
+        $html = $controller->getSettingsHeadline();
+        $this->assertMatchesHtmlSnapshot($html);
+    }
+
+    public function testSettings()
+    {
+        $controller = new YoutubeEmbed();
+        $html = $controller->settings();
+        $this->assertStringContainsStringIgnoringCase(
+            'data-thumbnail="content/modules/youtube_embed/images/player.jpg"',
+            $html
+        );
+    }
+
+    public function testHead()
+    {
+        $output = TestHelper::getOutput(function () {
+            $controller = new YoutubeEmbed();
+            $controller->head();
+        });
+        $this->assertStringStartsWith(
+            '<link rel="stylesheet" href="content/cache/stylesheets/',
+            $output
+        );
+    }
+
+    public function testAdminHead()
+    {
+        $output = TestHelper::getOutput(function () {
+            $controller = new YoutubeEmbed();
+            $controller->adminHead();
+        });
+        $this->assertStringStartsWith(
+            '<link rel="stylesheet" href="content/cache/stylesheets/',
+            $output
+        );
+    }
+
+    public function testThumbnailReturnsNull()
+    {
+        $controller = new YoutubeEmbed();
+        $this->assertNull($controller->_thumbnail());
+    }
+
+    public function testThumbnailReturnsString()
+    {
+        $_GET["url"] = "https://youtu.be/qyqWGSmo9PY";
+        $_GET["number"] = "2";
+        $controller = new YoutubeEmbed();
+        $this->assertMatchesSnapshot($controller->_thumbnail());
+    }
 }
